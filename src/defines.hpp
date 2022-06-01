425e57d495852b7fb3092144e50a7f5dc15c4ae2
Lewis <vk2gzz@ieee.org>
2022-06-01 12:28:52 +1000

    Stuff

--- src/defines.hpp
+++ src/defines.hpp
@@ -6,5 +6,13 @@
 
 #define I2C_SDA 0
 #define I2C_SCL 1
+#define UART_TX 28
+#define UART_RX 29
+#define TX_EN 24
+#define D_OUT 25
+#define HB 23
+
+#define GPS_BAUD 9600
+#define FSK_BAUD 300
 
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -13,8 +13,10 @@
 #include "defines.hpp"
 #include "Logger.hpp"
 #include "si5351.h"
+#include "TinyGPS++.h"
 
 Si5351 rf;
+TinyGPSPlus gps;
 
 // CORE 0 Responsible for Serial prompt.
 void setup() 
@@ -26,28 +28,51 @@ void setup()
 
 void loop() 
 {
-
+    //SHELL
+    //HEARTBEAT
 }
 
 
 // CORE 1 Responsible for GPS and Shifting data.
 void setup1()
 {
+    delay(5000); // WAIT FOR CORE 1 to START
     Logger log("IOSETUP", INFO);
+    
+    
     bool rf_conn;
     log.Send(INFO, "SETTING I2C PINS > ", I2C_SDA, I2C_SCL);
     Wire.setSDA(I2C_SDA);
     Wire.setSCL(I2C_SCL);
+    log.Send(INFO, "CONNECTING TO RF CHIP");
     rf_conn = rf.init(SI5351_CRYSTAL_LOAD_8PF, 0, 0);
     if(!rf_conn)
     {
-        log.Send(FATAL, "COULD NOT CONNECT TO Si5351");
+       log.Send(FATAL, "COULD NOT CONNECT TO RF CHIP");
     }
-    
-    
+    log.Send(INFO, "CONNECTED TO RF CHIP");
+    log.Send(INFO, "SETTING UP RF");
+    // rf.set_freq(FREQ_0, SI5351_CLK0);
+    // rf.set_freq(FREQ_1, SI5351_CLK1);
+    pinMode(TX_EN, OUTPUT);
+    pinMode(D_OUT, OUTPUT);
+
+
+
+    log.Send(INFO, "STARTING UART PINS > ", UART_TX, UART_RX);
+    Serial1.setTX(UART_TX);
+    Serial1.setRX(UART_RX);  
+    Serial1.begin(GPS_BAUD);
+
 }
 
 void loop1()
 {
-
+    Logger log("IOLOOP", WARNING);
+    for(;;)
+    {
+        while(Serial1.available() > 0)
+            gps.encode(Serial1.read());
+        
+    }
 }
