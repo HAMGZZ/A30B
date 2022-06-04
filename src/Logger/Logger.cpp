c764191dee5e865ac6da919807b5d6cc806fa4e8
Lewis <vk2gzz@ieee.org>
2022-06-04 10:51:01 +1000

    Added logger Dir

---
+++ src/Logger/Logger.cpp
@@ -0,0 +1,99 @@
+#include "Logger/Logger.hpp"
+#include "defines.hpp"
+
+Logger::Logger()
+{
+    name = new (char[30]);
+}
+
+Logger::Logger(char const *systemName, Level minLogLevelSet)
+{
+    name = new (char[30]);
+    strcpy(name, systemName);
+    minLogLevel = minLogLevelSet;
+}
+
+
+char const *Logger::enum_to_string(Level type) {
+   switch(type) {
+      case DEBUG:
+         return "DEBUG";
+      case INFO:
+         return "INFO";
+      case WARNING:
+         return "WARNING";
+      case ERROR:
+         return "ERROR";
+      case FATAL:
+         return "FATAL";
+   }
+   return "";
+}
+
+void Logger::Start(char const *systemName, Level minLogLevelSet)
+{
+    strcpy(name, systemName);
+    minLogLevel = minLogLevelSet;
+}
+
+void Logger::Send(Level level, const char *message, float var1, float var2)
+{
+    
+    if(level < minLogLevel)
+        return;
+
+    if(var1 == 0 && var2 == 0)
+    {
+        Serial.printf("LOG <<%d>>[%s %0.1lf][%s][%s]::  %s\n\r", millis(), PROG_NAME, VERSION, name, enum_to_string(level), message);
+    }
+
+    else if(var2 == 0)
+    {
+        Serial.printf("LOG <<%d>>[%s %0.1lf][%s][%s]::  %s %lf\n\r", millis(), PROG_NAME, VERSION, name, enum_to_string(level), message, var1);
+    }
+    else
+    {
+        Serial.printf("LOG <<%d>>[%s %0.1lf][%s][%s]::  %s %lf %lf\n\r", millis(), PROG_NAME, VERSION, name, enum_to_string(level), message, var1, var2);
+    }
+    
+    if(level == FATAL)
+    {
+        Serial.printf("HALTING...\r\n");
+        for(;;){}
+    }
+
+}
+
+void Logger::Send(Level level, const char *message, bool var1)
+{
+    if(level < minLogLevel)
+        return;
+
+    Serial.printf("LOG <<%d>>[%s %0.1lf][%s][%s]::  %s %s\n\r", millis(), PROG_NAME, VERSION, name, enum_to_string(level), message, var1 ? "True" : "False");
+    
+    if(level == FATAL)
+    {
+        Serial.printf("HALTING...\r\n");
+        for(;;){}
+    }
+}
+
+void Logger::Send(Level level, char const *message, char *message2)
+{
+    if(level < minLogLevel)
+        return;
+
+    Serial.printf("LOG <<%d>>[%s %0.1lf][%s][%s]::  %s %s\n\r", millis(), PROG_NAME, VERSION, name, enum_to_string(level), message, message2);
+    
+    if(level == FATAL)
+    {
+        Serial.printf("HALTING...\r\n");
+        for(;;){}
+    }
+}
+
+
+Logger::~Logger()
+{
+    delete[] name;
+}
---
+++ src/Logger/Logger.hpp
@@ -0,0 +1,35 @@
+#ifndef LOGGER_HPP
+#define LOGGER_HPP
+
+#include <Arduino.h>
+
+
+typedef enum
+{
+    DEBUG,
+    INFO,
+    WARNING,
+    ERROR,
+    FATAL
+} Level;
+
+class Logger
+{
+private:
+    char *name;
+    Level minLogLevel;
+    char const *enum_to_string(Level type);
+    //time_t getTeensyTime();
+public:
+    Logger();
+    Logger(char const *SystemName, Level minLogLevelSet);
+    void Start(char const *SystemName, Level minLogLevelSet);
+    void Send(Level level, char const *message, float var1 = (0.0F), float var2 = (0.0F));
+    void Send(Level level, char const *message, bool var1);
+    void Send(Level level, char const *message, char *message2);
+    ~Logger();
+};
+
+
+
+#endif
