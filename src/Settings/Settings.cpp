c70fa064ed4d4f70009c4c620810572dc8a13011
Lewis <lewis@czgzz.space>
2022-09-03 12:48:49 +1000

    Added settings class

---
+++ src/Settings/Settings.cpp
@@ -0,0 +1,50 @@
+#include "Settings.hpp"
+
+int Settings::Init()
+{
+    Serial.printf("READING SETTINGS FILE ...[ ]");
+    if(Read())
+    {
+        Serial.printf("\b\bWARNING]\r\n");
+        Serial.printf("Error reading file... Using defaults!\r\n");
+        strcpy(Callsign, DEFAULT_CALL);
+        strcpy(Icon, DEFAULT_ICON);
+        CentreFrequency = DEFAULT_CTR_FRQ;
+        FrequencyShift = DEFAULT_SHIFT;
+        BaudRate = DEFAULT_BAUD;
+        TransmissionDelay = DEFAULT_TR;
+        ShellColour = 1;
+        Serial.printf("=== PLEASE RUN 'save' TO SAVE SETTINGS ===\r\n");
+    }
+    else
+        Serial.printf("\b\bOK]\r\n");    
+    return 0;
+}
+
+int Settings::Read()
+{
+    fs:File file = LittleFS.open(SETTINGS_PATH, "r");
+    if(!file)
+      return 1;
+    char in[MAXCHAR] = {0};
+    int count = SETTINGS_STORED;
+    file.readBytesUntil('\n', in, MAXCHAR);
+    sscanf(in, "%s", Callsign);
+    memset(in, 0, MAXCHAR);
+    file.readBytesUntil('\n', in, MAXCHAR);
+    sscanf(in, "%s", Icon);
+    memset(in, 0, MAXCHAR);
+    file.readBytesUntil('\n', in, MAXCHAR);
+    sscanf(in, "%llu,%llu,%lu,%lu,%d", &CentreFrequency, &FrequencyShift, &BaudRate, &TransmissionDelay, &ShellColour);
+    memset(in, 0, MAXCHAR);
+    file.close();
+    return 0;
+}
+
+int Settings::Write()
+{
+    fs:File file = LittleFS.open(SETTINGS_PATH, "w");
+    file.printf("%s\n%s\n%llu,%llu,%lu,%lu,%d\n\r", Callsign, Icon, CentreFrequency, FrequencyShift, BaudRate, TransmissionDelay, ShellColour);
+    file.close();
+    return 0;
+}
---
+++ src/Settings/Settings.hpp
@@ -0,0 +1,30 @@
+#ifndef SETTINGS_HPP
+#define SETTINGS_HPP
+
+#include <Arduino.h>
+#include <LittleFS.h>
+#include "tools/tools.hpp"
+#include "defines.hpp"
+
+
+
+class Settings
+{
+private: 
+public:
+
+    char Callsign[CALLSIGN_SIZE];
+    char Icon[ICON_SIZE];
+    unsigned long long ZeroFreq;
+    unsigned long long OneFreq;
+    unsigned long BaudRate;
+    unsigned long TransmissionDelay;
+    bool ShellColour;
+
+    int Init();
+    int Write();
+    int Read();
+    int defaultConfig();
+};
+
+#endif
